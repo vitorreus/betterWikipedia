0536736968f3373cade03b151ca1390e92c4695d
Vitor <vitor.reus@gmail.com>
2014-04-19 22:11:47 +0200

    first partially working version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 betterWikipedia
 ===============
+Better wikipedia reading based on this http://www.gizmodo.co.uk/2014/04/i-wish-i-could-read-wikipedia-like-this/
+
+This is a work in progress.
+
+TODO: 
+* Middle-mouse clicks and similar actions to open link in new tab should open them in new tab 
+* Do a better query of the wikipedia API
+* Nested queries
+* Expand current content
+* Use fancy inlining of content instead of windows
--- betterWikipedia.user.js
+++ betterWikipedia.user.js
@@ -9,12 +9,104 @@
 
 
 $(window).hashchange(hashchanged); 
+ 
+
+function handleAjaxResponse(content){
+
+}
+
+
+function parseAjaxResponse(content,windowId){
+	//put it on the mouse pos haha.
+     console.log(content);
+    var pageId = 0;
+    for(pageId in content.query.pages){}
+    
+    console.log(content.query.pages[pageId].extract);
+    
+    $("#" +windowId).html(content.query.pages[pageId].extract);
+}
+
+//gah
+var mouse = {x:0,y:0};
+$("body").mousemove(function(e) {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+    //console.log(mouse);
+})
+
+
+openwindows = [];
+$("body").click(function(){
+    console.log("close window")
+    for (var i in openwindows){
+    	$(openwindows[i]).remove();
+    }
+
+});
+function createWindow(){
+	//create a new div and return its id to put the ajax content in here afterwards. Also put a link and stuff.
+    
+    var div = $("<div>");
+    $("body").append(div);
+    
+    $(div).click(function(e){e.stopPropagation();console.log("dont cose windos")});
+    
+    var id = "_w"+ Math.floor(Math.random() * 100000);
+    
+    div.attr("id",id);
+    openwindows.push(div);
+    div.css({
+        position:"absolute",
+        width:"300px",
+        height:"100px",
+        top:mouse.y+20,
+        left:mouse.x-100,
+        background:"white",
+        border:"1px solid black",
+        padding:"5px",
+        "font-size":"12px"
+        
+        
+    })
+    return id;
+}
+
+
+function loadContent(title){
+     
+	//var url = "/w/api.php?action=parse&page="+title+"&format=json&prop=text&section=1";
+    
+    var url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exchars=200&titles="+title+"&format=json";
+    
+    
+    
+    
+    //open a window
+    var windowId = createWindow()
+    
+    $.ajax({
+        url:url,
+        success:function (wid){
+            console.log("returning function for wid="+wid)
+            return function(d){
+                parseAjaxResponse(d,wid)
+            }
+        }(windowId)
+    });
+                      
+                       
+}
+
+
+
 
 function hashchanged(){
     
     var hash = window.location.hash.split("/");
     if (hash[1] == "wiki"){
-		console.log("load" + window.location.hash );
+		console.log("load" + window.location.hash ); 
+        loadContent(hash[2]);
     }
 }
 
@@ -22,5 +114,6 @@ $("#bodyContent").find("a").each(function(){
     var href = $(this).attr("href");
     if (href.charAt(0) != "#"){
 		$(this).attr("href","#" + href);
+        //also change the onmidle mouse button maybe...
     }
 })
