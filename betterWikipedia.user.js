57e4cb7654bb0897fc6de5bda089189e7170f660
Vitor <vitor.reus@gmail.com>
2014-04-19 20:18:29 +0200

    header

--- betterWikipedia.user.js
+++ betterWikipedia.user.js
@@ -1,1 +1,9 @@
+// ==UserScript==
+// @name       Better Wikipedia
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      ://*.wikipedia.org/*
+// @copyright  2012+, Vitor
+// ==/UserScript==
 console.log("This will be the better wikipedia reading based on this http://www.gizmodo.co.uk/2014/04/i-wish-i-could-read-wikipedia-like-this/");
